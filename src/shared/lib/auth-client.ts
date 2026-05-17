503b07ccec84a08c0b1cec9e05bbf2de046b72ca
elvizsky-23 <elvizskuy@gmail.com>
2026-05-17 14:32:40 +0800

    fix 1

--- src/shared/lib/auth-client.ts
+++ src/shared/lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react";
-import { env } from "@/shared/config/env";
 
 export const authClient = createAuthClient({
-	baseURL: env.BETTER_AUTH_URL,
+	baseURL: process.env.BETTER_AUTH_URL || "",
 });
 
 export const { signUp, signOut, signIn, useSession } = authClient;
--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,15 +1,13 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { createServerOnlyFn } from "@tanstack/react-start";
 import { PrismaClient } from "@/generated/prisma/client";
-import { env } from "@/shared/config/env";
 
-export const getConnectionString = createServerOnlyFn(() => {
-	const connectionString = env.DATABASE_URL;
+function getConnectionString(): string {
+	const connectionString = process.env.DATABASE_URL;
 	if (!connectionString) {
 		throw new Error("DATABASE_URL is not set");
 	}
 	return connectionString;
-});
+}
 
 const connectionString = getConnectionString();
 
